b27242f04bcdb10c5e09521baa4fc555bab45928
cosmix-ai <bahafitree@gmail.com>
2025-09-06 12:32:31 +0700

    Add new script

--- all-scripts.lua
+++ all-scripts.lua
@@ -2,3 +2,4 @@ title=99,script=
                                 loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/Vector_Hub/0/v2"))()
                             
 title=Hi,script=print("fi")
+title=Hi,script=print("fi")
